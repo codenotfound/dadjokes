3d9a87982c553ec85e2d4be1451b719e2ed0c31c
codenotfound <dmitry.velikij@gmail.com>
2022-01-04 19:47:44 +0300

    cleanup

---
+++ src/store/getter-types.ts
@@ -0,0 +1,2 @@
+export const RANDOM_DAD_JOKE = 'RANDOM_DAD_JOKE';
+export const SEARCHED_DAD_JOKE = 'SEARCHED_DAD_JOKE ';
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,20 @@
 import DadJokesService from '@/services/dadjokesservice';
 import { createStore } from 'vuex';
+import { RANDOM_DAD_JOKE } from './getter-types';
+import { SET_RANDOM_DAD_JOKE } from './mutation-types';
+import { GET_RANDOM_DAD_JOKE } from './action-types';
 
 export default createStore({
   state: {
     randomDadJoke: '',
   },
   mutations: {
-    SET_RANDOM_DAD_JOKE(state, payload) {
+    [SET_RANDOM_DAD_JOKE](state, payload) {
       state.randomDadJoke = payload;
     },
   },
   actions: {
-    GET_RANDOM_DAD_JOKE({ commit }) {
+    [GET_RANDOM_DAD_JOKE]({ commit }) {
       DadJokesService.getRandomDadJoke()
         .then((res) => {
           commit('SET_RANDOM_DAD_JOKE', res.data.joke);
@@ -19,7 +22,7 @@ export default createStore({
     },
   },
   getters: {
-    RANDOM_DAD_JOKE: (state) => state.randomDadJoke,
+    [RANDOM_DAD_JOKE]: (state) => state.randomDadJoke,
   },
   modules: {
   },
